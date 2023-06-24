a576a168a9e9157ad9058f9669cf3c161850df31
ganeshh24 <124494549+ganeshh24@users.noreply.github.com>
2023-06-24 21:18:54 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,14 @@
+﻿document.getElementById("index").addEventListener("click", function (event) {
+	event.preventDefault();
+
+	var username = document.getElementById("username");
+	var email = document.getElementById("email");
+	var password = document.getElementById("password");
+	var confirmpassword = document.getElementById("confirmpassword");
+	if (username.value === "" || email.value === "" || password.value === "" || confirmpassword.value === "") {
+		alert("fill all the requriments.");
+		return;
+	}
+	window.location.herf = "login.html";
+
+});
---
+++ profile.js
@@ -0,0 +1,12 @@
+﻿document.getElementById("signin").addEventListener("click", function (event) {
+	event.preventDefault();
+
+	var email = document.getElementById("email");
+	var password = document.getElementById("password");
+	if (email.value === "" || password.value === "") {
+		alert("fill all the requriments.");
+		return;
+	}
+	window.location.herf = "register.html";
+
+});
